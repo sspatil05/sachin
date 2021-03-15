1ede70f7c25537f1b91b1bad26a51d1425376f1c
sspatil05 <46047052+sspatil05@users.noreply.github.com>
2021-03-15 17:42:31 +0530

    Create ec2_create.groovy

---
+++ ec2_create.groovy
@@ -0,0 +1,48 @@
+branch = "master"
+git_url = "https://github.deere.com/parts/parts-hci-infra"
+
+freeStyleJob('create-update-ec2-stack-devl') {
+    logRotator(numToKeep = 100)
+    parameters {
+
+        stringParam("ENVIRONMENT", "devl")
+
+        choiceParam("OPERATIONS", ["dry_run","deploy","delete"])
+        choiceParam("RE_CREATE", ["false","true"])
+    }
+    scm {
+	    git {
+            remote {
+                  url(git_url)
+                 
+            }
+            branch(branch)
+        }
+    }
+    steps {
+        shell( '''
+
+
+
+if [ "$RE_CREATE" = false ]
+then
+   FORCE_DELETE=""
+else
+   FORCE_DELETE=--force_delete
+fi
+
+python --version
+python -m cloud_common.aws_stack_operation \\
+--region us-east-1 \\
+--role_name $ROLE_NAME \\
+--account_number $ACCOUNT_NUMBER \\
+--operation $OPERATIONS \\
+--arch $ARCH_FILE \\
+--var $VAR_FILE \\
+--stack_name $STACK_NAME \\
+--config_file $CONFIG_FILE \\
+--template_url $TEMPLATE_URL \\
+--log DEBUG $FORCE_DELETE $USE_EXISTING
+        ''')
+    }
+}
