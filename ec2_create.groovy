640130a63e1f7bc0fa18bfbd1f4ee97fd6eaaf43
sspatil05 <46047052+sspatil05@users.noreply.github.com>
2021-03-15 17:22:11 +0530

    Update ec2_create.groovy

--- ec2_create.groovy
+++ ec2_create.groovy
@@ -1,7 +1,7 @@
 import boto3
 ec2 = boto3.resource('ec2')
 
-# create a new EC2 instance
+#create a new EC2 instance
 instances = ec2.create_instances(
      ImageId='ami-0915bcb5fa77e4892',
      MinCount=1,
