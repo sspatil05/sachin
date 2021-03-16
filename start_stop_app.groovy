ab77e5f014532b704a5ac6c53cddd3e397dacce7
sspatil05 <46047052+sspatil05@users.noreply.github.com>
2021-03-16 17:52:17 +0530

    Create start_stop_app.groovy

---
+++ start_stop_app.groovy
@@ -0,0 +1,43 @@
+branch = "master"
+git_url = "https://github.com/sspatil05/sachin.git"
+
+
+freeStyleJob("start-stop-app-devl") {
+    description 'Deploys app to a given account'
+    logRotator(daysToKeep = -1, numToKeep = 10, artifactDaysToKeep = -1, artifactNumToKeep = -1)
+    label('devl-slave')
+
+	scm {
+        git {
+            remote {
+                url("${git_url}")
+                
+            }
+            branch("${branch}")
+            extensions {
+            }
+        }
+
+    }
+    parameters {
+            choiceParam("OPERATION", ["start","stop","restart"])
+      		  choiceParam("NODE_GROUP",["  ","app-EC2","coll-EC2","app-EC2&coll-EC2"] )
+      		  stringParam('INSTANCEID', ' ', 'Copy Instance ID From AWS Console' )
+
+    }
+
+    wrappers {
+      preBuildCleanup()
+      maskPasswords()
+    }
+    steps {
+        shell('''#!/bin/bash
+    set -e
+
+
+
+
+
+		''')
+   }
+}
